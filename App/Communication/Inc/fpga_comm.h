a12367f8a6840cfeccdc86d1f87d676a220bd4e0
BaekKim0909 <742227049@qq.com>
2026-07-31 15:21:10 +0800

    7.31提交

--- App/Communication/Inc/fpga_comm.h
+++ App/Communication/Inc/fpga_comm.h
@@ -5,10 +5,10 @@
 #ifndef YOUJIESUN_FPGA_COMM_H
 #define YOUJIESUN_FPGA_COMM_H
 
-#define TEMPERATURE_REG             0x0101  /* 温度寄存器 */
-#define LID_STATE_REG               0x0105  /* 盖子状态寄存器地址 */
-#define OIL_POUR_STATE_REG          0x0106  /* 排油状态寄存器地址 */
-#define OIL_CUP_STATE_REG           0x0107  /* 油杯状态寄存器地址 */
+#define TEMPERATURE_REG             (0x0101U)  /* 温度寄存器 */
+#define LID_STATE_REG               (0x0105U)  /* 盖子状态寄存器地址 */
+#define OIL_POUR_STATE_REG          (0x0106U)  /* 排油状态寄存器地址 */
+#define OIL_CUP_STATE_REG           (0x0107U)  /* 油杯状态寄存器地址 */
 
 #define FPGA_COMM_REGISTER_SIZE                 (2U)    /* 寄存器大小为两个字节 */
 #define FPGA_COMM_REGISTER_DATA_START_INDEX     (6U)    /* 指令数据内容起始index */
